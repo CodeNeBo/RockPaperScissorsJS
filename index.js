42eed94f92f23b7cff65e3f6a9f084bd1e65be62
CodeNeBo <nechitaylob@gmail.com>
2022-07-25 14:54:07 +0300

    Added favicon, TODO list and optimized func

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 console.log("Hello handsome!")
 
-const computerChoise = Array("Rock", "Paper", "Scissors");
-
-function getComputerChoice () {
-    let result = computerChoise[Math.floor(Math.random()*computerChoise.length)];
-    return result;
+function getComputerChoise() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
-console.log(getComputerChoice)
+// TODO 
+// 1. Write a func that starts the game 
+// 2. Write a func that makes player to choose the fighter 
+// 3. Write a func that makes comp choose the fighter 
+// 4. Write a func that evaluates the fighters and choose a winner 
+// 5. Write a func that counts who win 
+// 6. Fix bugs 
+
+function getPlayerSelection (){
+    //WIP
+}
+
+
+function playRound (playerSelection, computerSelection) {
+    //WIP
+};
