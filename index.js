afb7c7e6281f32b6036cda5c1e181b2ed6568a65
CodeNeBo <nechitaylob@gmail.com>
2022-07-25 15:19:01 +0300

    Added win conditions

--- index.js
+++ index.js
@@ -8,16 +8,22 @@ function getComputerChoise() {
 // TODO 
 // 1. Write a func that starts the game 
 // 2. Write a func that makes player to choose the fighter 
-// 3. Write a func that makes comp choose the fighter 
-// 4. Write a func that evaluates the fighters and choose a winner 
+// 3. DONE Write a func that makes comp choose the fighter 
+// 4. DONE Write a func that evaluates the fighters and choose a winner 
 // 5. Write a func that counts who win 
 // 6. Fix bugs 
 
-function getPlayerSelection (){
-    //WIP
-}
-
-
 function playRound (playerSelection, computerSelection) {
-    //WIP
+    let playerPoints = 0; 
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+    (playerSelection == 'paper' && computerSelection == 'rock') || 
+    (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        playerPoints = 1;
+        console.log('You win! ' + playerSelection.charAt(0).toUpperCase() + ' beats ' + computerSelection + '!')
+    } else {
+        computerPoints = 1;
+        console.log('You lose! ' + computerSelection.charAt(0).toUpperCase() + ' beats ' + playerSelection + '!')
+    }
+    return playerPoints;
 };
