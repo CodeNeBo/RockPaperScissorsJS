1e9f4486dd862021e9baab2ae83f83278adf7682
CodeNeBo <nechitaylob@gmail.com>
2022-08-16 00:25:06 +0300

    First working vesion

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ let computerScore = 0;
 
 let body = document.querySelector("body");
 
+let welcomeText = document.createElement("h1"); 
+welcomeText.className = "headText";
+welcomeText.textContent = "Welcome to very cool Rock Paper Scissors battle!";
+document.body.appendChild(welcomeText);
+
+let scoreDiv = document.createElement("div");
+scoreDiv.className = "scoreDiv";
+document.body.appendChild(scoreDiv);
+
+let dialogDiv = document.createElement("div");
+dialogDiv.className = "dialogDiv";
+document.body.appendChild(dialogDiv);
+
 let buttons = document.createElement('div');
 buttons.className = "buttons"
 document.body.appendChild(buttons);
@@ -29,34 +42,43 @@ function getComputerChoise() {
     return choices[Math.floor(Math.random() * choices.length)]
 };
 
+
+
 function playRound (playerSelection) {
     let computerSelection = getComputerChoise(); 
     let result = "";
-
     
     if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
         (playerSelection == 'paper' && computerSelection == 'rock') || 
         (playerSelection == 'scissors' && computerSelection == 'paper')) {
             playerScore += 1;
-            console.log('You win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + ' beats ' + computerSelection + '!')
+            result = ('You win! ' + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + ' beats ' + computerSelection + '!')
 
             if (playerScore == 5){
-                console.log("Congratulations! You won! Press any button to play again.")
+                result = ("Congratulations! You won! Press any button to play again.")
                 playerScore = 0; 
                 computerScore = 0;
             }
     } else if (playerSelection == computerSelection) {
-        console.log("You both picked " + playerSelection + "... It's a tie!");
+        result = ("You both picked " + playerSelection + "... It's a tie!");
     } else {
         computerScore += 1; 
-        console.log('You lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection + '!')
+        result = ('You lose! ' + computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1) + ' beats ' + playerSelection + '!')
 
         if (computerScore == 5){
-            console.log("You lost! Press any button to play again.")
+            result = ("You lost! Press any button to play again.")
             playerScore = 0; 
             computerScore = 0;
         }
     }
+
+    let currentPlayerScore = ("Player score: " + playerScore);
+    let currentComputerScore = ("Computer score: " + computerScore);
+    currentScore = `<div>${currentPlayerScore}</div><br><div>${currentComputerScore}</div>`
+    scoreDiv.innerHTML = currentScore;
+
+    document.querySelector('.dialogDiv').innerHTML = result;
+    return
 };
 
 btn.forEach(button =>{
